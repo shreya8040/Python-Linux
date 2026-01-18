6c9c0ef99a432b551bdd7f67fe4161f4a359b3fd
shreya8040 <shreya8040@gmail.com>
2026-01-18 13:14:14 +0530

    monitor vlatest

--- monitor.py
+++ monitor.py
@@ -1,19 +1,14 @@
 from textout import t
 import plotext as plt
-<<<<<<< HEAD
-l=len(t)
-v=[l]
-v=t
-s = [100,100,100,100,100,100,100,100]
-z= plt.bar(v,s)
-=======
+
+
 v=list(range(len(t)))
 s=[100]*len(t)
 
 plt.canvas_color('black')
-plt.plot_size(100,20)
+
 plt.ylim(0,100)
 
 plt.bar(v,t)
->>>>>>> b198608 (update)
+
 plt.show()
